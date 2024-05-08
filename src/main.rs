32d094bce31b614eb059360003387e6d2dd8e675
Uday Patel <uday3patel@gmail.com>
2024-05-08 10:14:31 -0400

    feat: Parsed JSON to build expected TUI output

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,33 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get(url).await?;
     let json: Value = response.json().await?;
 
-    let game_data = json.get("scoreboard").unwrap();
+    let scoreboard = json.get("scoreboard").unwrap();
+    let game_data = scoreboard.get("games").unwrap();
 
-    println!("Data: {}", game_data);
+    for game in game_data.as_array().ok_or("Expected an array")? {
+        
+        let away_team = game.get("awayTeam").unwrap();
+        let home_team = game.get("homeTeam").unwrap();
+
+        let away_team_tricode = away_team.get("teamTricode").unwrap();
+        let away_team_score = away_team.get("score").unwrap();
+        let home_team_tricode = home_team.get("teamTricode").unwrap();
+        let home_team_score = home_team.get("score").unwrap();
+
+        let game_status = game.get("gameStatusText").unwrap();
+
+        println!("🏀 {}: {} - {}: {} ({})", 
+            away_team_tricode.as_str().unwrap(), 
+            away_team_score, 
+            home_team_tricode.as_str().unwrap(), 
+            home_team_score,
+            game_status.as_str().unwrap()
+        );
+    }
+
+
+
+    // println!("Data: {}", game_data);
 
     Ok(())
 
