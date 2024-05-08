774b7ca83f52c1c49732f57169ffd83d658a0a30
Uday Patel <uday3patel@gmail.com>
2024-05-07 23:45:39 -0400

    feat: able to access game data from API endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest;
+use serde_json::{self, Value};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let url = "https://nba-prod-us-east-1-mediaops-stats.s3.amazonaws.com/NBA/liveData/scoreboard/todaysScoreboard_00.json";
+
+    let response = reqwest::get(url).await?;
+
+    let json: Value = response.json().await?;
+
+    let game_data = json.get("scoreboard").unwrap();
+
+    println!("Data: {}", game_data);
+
+    Ok(())
+
 }
+
+
+// fn main() {
+//     println!("Hello, world!");
+// }
