1e0d87d9441fe30beb5c7a87f784e14be8910e26
Uday Patel <uday3patel@gmail.com>
2024-05-12 16:29:22 -0400

    feat: refactored with match command to reduce future tech debt when adding more cli args, making it extensible

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod args;
 mod utils;
 
+use args::CliArgs;
 use reqwest;
 use serde_json::{self, Value};
 use tokio;
-
+use colored::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,32 +19,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let game_data = scoreboard.get("games").unwrap();
 
     let cli_args = args::parse_arguments();
-    let spotlight = cli_args.team_spotlight;
 
     if game_data.as_array().unwrap().len() > 0 {
 
-        if spotlight.is_some() {
+        match cli_args {
 
-            for game in game_data.as_array().ok_or("Expected an array")? {
-        
-                let away_team = game.get("awayTeam").unwrap();
-                let home_team = game.get("homeTeam").unwrap();
-        
-                let away_team_tricode = away_team.get("teamTricode").unwrap();
-                let home_team_tricode = home_team.get("teamTricode").unwrap();
-    
-                if spotlight.clone().unwrap().to_uppercase().eq(away_team_tricode) || spotlight.clone().unwrap().to_uppercase().eq(home_team_tricode) {
-                    utils::display_per_game(game);
-                    break;
-                }
-            }
-        }
-        else {
+            CliArgs {
+                team_spotlight: Some(spotlight_tricode)
+            } => {
+                let mut is_found = false;
+
+                for game in game_data.as_array().ok_or("Game Data JSON is Unstructured")? {
+                    let away_team = game.get("awayTeam").unwrap();
+                    let home_team = game.get("homeTeam").unwrap();
             
-            for game in game_data.as_array().ok_or("Expected as array")? {
-                utils::display_per_game(game);
-            }
+                    let away_team_tricode = away_team.get("teamTricode").unwrap();
+                    let home_team_tricode = home_team.get("teamTricode").unwrap();
+
+                    if spotlight_tricode.to_uppercase().eq(away_team_tricode) || spotlight_tricode.to_uppercase().eq(home_team_tricode) {
+                        is_found = true;
+                        utils::display_per_game(game);
+                        break;
+                    }
+                }
+
+                if !is_found { println!("😔 Team data for Tricode [{}] is Unavailable", spotlight_tricode.bold()); }
+            },
+
+            CliArgs {
+                team_spotlight: None
+            } => utils::display_all_games(json),
         }
+
     } 
     else {
         println!("😔 There are no games at the moment");
--- src/utils.rs
+++ src/utils.rs
@@ -23,3 +23,33 @@ pub fn display_per_game(game: &Value) {
         game_status.as_str().unwrap().trim().yellow()
     );
 }
+
+pub fn display_all_games(json: Value) {
+
+    let scoreboard = json.get("scoreboard").unwrap();
+    let game_data = scoreboard.get("games").unwrap();
+
+    for game in game_data.as_array().unwrap() {
+
+        let away_team = game.get("awayTeam").unwrap();
+        let home_team = game.get("homeTeam").unwrap();
+
+        let away_team_tricode = away_team.get("teamTricode").unwrap();
+        let away_team_score = away_team.get("score").unwrap();
+
+        let home_team_tricode = home_team.get("teamTricode").unwrap();
+        let home_team_score = home_team.get("score").unwrap();
+
+        let game_status = game.get("gameStatusText").unwrap();
+
+        println!("🏀 {}: {} - {}: {} ({})", 
+            away_team_tricode.as_str().unwrap().bold(),
+            away_team_score.to_string().bright_blue(),
+            home_team_tricode.as_str().unwrap().bold(),
+            home_team_score.to_string().bright_blue(),
+            game_status.as_str().unwrap().trim().yellow()
+        );
+
+    }
+    
+}
